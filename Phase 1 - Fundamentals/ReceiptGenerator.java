0963c3deffe3cdea08a49f1f607f6986b32425c3
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-05-11 20:04:04 +0100

    Finished reciept generator. I would like to come back to add loops and such, or recreater as v2 later. I still find printf awkward. The rest was okay.

--- Phase 1 - Fundamentals/ReceiptGenerator.java
+++ Phase 1 - Fundamentals/ReceiptGenerator.java
@@ -5,15 +5,26 @@ public class ReceiptGenerator {
         
         String itemName;
         double itemPrice = 0;
-        double itemTax = 0;
-        double total;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Type in the items name: ");
         itemName = scanner.nextLine();
 
-        
+        System.out.println("Type in the items price: ");
+        itemPrice = scanner.nextDouble();
+
+        scanner.close();
+
+        double itemTax = itemPrice * 0.2;
+        double total = itemPrice + itemTax;
+
+        System.out.println("The tax is 20%");
+
+        System.out.println("I am printing reciept now.");
+
+        System.out.println("Item: " + itemName);
+        System.out.printf("Price: %.2f\nTax: %.2f\nTotal: %.2f",itemPrice, itemTax, total);
 
     } 
 }
