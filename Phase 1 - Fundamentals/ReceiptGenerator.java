ca529ee2c5f0cea1070bbf10ebe7407e40a66251
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-05-11 17:57:18 +0100

    Working on proejct. learned when using scanner with a String you must use nextLine.

---
+++ Phase 1 - Fundamentals/ReceiptGenerator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReceiptGenerator {
+    public static void main(String[] args){
+        
+        String itemName;
+        double itemPrice = 0;
+        double itemTax = 0;
+        double total;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type in the items name: ");
+        itemName = scanner.nextLine();
+
+        
+
+    } 
+}
