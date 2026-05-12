c61c3dcbcf6419053c07f21531678977ce7f4db6
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-05-12 18:56:12 +0100

    Changed some spelling.

--- Phase 1 - Fundamentals/AverageOfThreeNumbers.java
+++ Phase 1 - Fundamentals/AverageOfThreeNumbers.java
@@ -14,6 +14,8 @@ public class AverageOfThreeNumbers{
         System.out.print("Enter a third number: ");
         double numberThree = scanner.nextDouble();
 
+        scanner.close();
+
         double average = ( numberOne + numberTwo + numberThree )/3;
 
         System.out.printf("The average of the numbers you have choosen is: %.2f", average);
