6fdc9a030dd773a77cb67c1d835a5281980e5b26
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-05-13 15:47:09 +0100

    finished this task. easy enough

---
+++ Phase 1 - Fundamentals/SplitBillCalculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SplitBillCalculator {
+    public static void main(String[] args){
+
+        int people;
+        double bill;
+        double tip;
+        double billSplit;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of people: ");
+        people = scanner.nextInt();
+
+        System.out.print("Enter bill: ");
+        bill = scanner.nextDouble();
+
+        scanner.close();
+
+        tip = bill * 0.1;
+        billSplit = (bill + tip)/people;
+
+        System.out.println(tip);
+        System.out.printf("The bill for each person is: %.2f", billSplit);
+    }
+}
