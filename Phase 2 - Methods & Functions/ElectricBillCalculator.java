d3ad2b00767b072cc013a389f1d3846f94346976
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-07-04 12:50:24 +0100

    I'm understand methods now.

--- Phase 2 - Methods & Functions/ElectricBillCalculator.java
+++ Phase 2 - Methods & Functions/ElectricBillCalculator.java
@@ -15,14 +15,35 @@ public class ElectricBillCalculator {
         scanner.close();
 
         double electricCost = calculateEnergyCharge(electricUsed);
+        double standingCharge = caclculateStandingCharge();
+        double subtotal = electricCost + caclculateStandingCharge();
+        double vatAmount = calculateVat(subtotal);
+        double finalBill = calculateTotalBill(subtotal, vatAmount);
+
+        System.out.println("Customer: " + customerName);
+        System.out.printf("%nEnergy Charge : %.2f%nStandingCharge : %.2f%nVat : %.2f%n--------------------%nTotal Bill : %.2f", electricCost, standingCharge, vatAmount, finalBill);
     }
 
-     public static double calculateEnergyCharge(double electricUsed){
+    public static double calculateEnergyCharge(double electricUsed){
 
         double unitCost = 0.37;
-
-        double electricCost = electricUsed * unitCost;
-
-        return electricCost;
+        return electricUsed * unitCost;
     }
+
+    public static double caclculateStandingCharge(){
+        
+        return 12.50;
+    }
+
+    public static double calculateVat(double subtotal){
+
+        return subtotal * 0.135;
+    }
+
+    public static double calculateTotalBill(double subtotal, double vatAmount){
+
+        return subtotal + vatAmount;
+    }
+
+
 }
