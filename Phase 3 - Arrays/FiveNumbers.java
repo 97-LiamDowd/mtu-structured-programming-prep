843935b52bdc9eb3e7457a241128d3827fcd169f
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-07-06 15:26:53 +0100

    started learning arrays

---
+++ Phase 3 - Arrays/FiveNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FiveNumbers {
+    public static void main(String[] args){
+
+    int[] fiveNumbers;
+    Scanner scanner = new Scanner(System.in);
+
+    for(int i = 0; i < 5; i++){
+        
+        System.out.println("Enter nuumber:");
+        fiveNumbers = scanner.nextInt();
+
+        }
+    }
+}
