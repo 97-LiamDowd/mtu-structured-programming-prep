92878b14a4d7b1bda714351cab058a54325ac428
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-07-07 13:44:33 +0100

    Hard

--- Phase 3 - Arrays/FiveNumbers.java
+++ Phase 3 - Arrays/FiveNumbers.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 public class FiveNumbers {
     public static void main(String[] args){
 
-    int[] fiveNumbers;
+    int[] fiveNumbers = new int[5];
     Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter five number:");
 
     for(int i = 0; i < 5; i++){
         
-        System.out.println("Enter nuumber:");
-        fiveNumbers = scanner.nextInt();
-
+        fiveNumbers[i] = scanner.nextInt();
         }
+
+    System.out.println("Your numbers are: ");
+
+    for(int i = 0; i < 5; i++){
+
+        System.out.println(fiveNumbers[i]);
+        }
+
+    scanner.close();
+    
     }
 }
