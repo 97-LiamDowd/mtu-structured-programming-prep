b5bf7a83daabb303f69d2bf831e046db0bf199d5
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-07-24 12:54:23 +0100

    easy

--- Phase 5 - Validation Loops/WithdrawalValidationPractice.java
+++ Phase 5 - Validation Loops/WithdrawalValidationPractice.java
@@ -9,12 +9,12 @@ public class WithdrawalValidationPractice {
         System.out.println("Enter withdrawal amount:");
         amount = scanner.nextDouble();
 
-        while (amount <= 0 && amount > balance) {
+        while (amount <= 0 || amount > balance) {
             System.out.println("Invalid amount. Enter a value greater than 0 and no more than your balance:");
             amount = scanner.nextDouble();
         }
         scanner.close();
-        
+
         balance = balance - amount;
 
         System.out.println("Withdrawal accepted.\nNew balance: " + balance);
