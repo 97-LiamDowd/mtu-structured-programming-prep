9079cb478e12621d70fa434f98b2c9c656e583c7
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-07-24 11:54:41 +0100

    Okay

--- Phase 5 - Validation Loops/MenuValidationPractice.java
+++ Phase 5 - Validation Loops/MenuValidationPractice.java
@@ -4,14 +4,14 @@ public class MenuValidationPractice {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        int inputOption=0;
+        int inputOption;
 
         System.out.println("Choose an option:\n1. Balance\n2. Withdraw\n3. Deposit\n4. Exit");
-        
+        inputOption = scanner.nextInt();
 
         while (inputOption < 1 || inputOption > 4) {
-            inputOption = scanner.nextInt();
             System.out.println("Invalid option, Choose 1-4:");
+            inputOption = scanner.nextInt();
         }
 
         System.out.println("You selected option " + inputOption + ".");
