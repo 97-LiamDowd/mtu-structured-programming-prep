27f090d464f0fcf487e0d486e96afc28088e5a52
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-07-31 16:54:30 +0100

    Learning about ArrayLists

--- Phase 6 - ArrayLists/ArrayListNamePractice.java
+++ Phase 6 - ArrayLists/ArrayListNamePractice.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class ArrayListNamePractice {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+
+        System.out.println("Enter five name:");
+
+        for (int i = 0; i < 5; i++) {
+            String name = scanner.nextLine();
+            names.add(name); 
+        }
+
+        System.out.println("Names entered:");
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        scanner.close();
+    }
 }
