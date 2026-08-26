27832ad87fb1054cf0a537878bd349b67f28ef74
Liam Dowd <liam.jonathan.dowd@gmail.com>
2026-08-26 13:05:35 +0100

    Updated

--- Phase 1 - Fundamentals/WeeklyWageCalculator1.java
+++ Phase 1 - Fundamentals/WeeklyWageCalculator1.java
@@ -8,7 +8,6 @@ public class WeeklyWageCalculator1 {
 
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
-        scanner.nextLine();
 
         System.out.println("Enter hours worked:");
         double hoursWorked = scanner.nextDouble();
@@ -17,11 +16,16 @@ public class WeeklyWageCalculator1 {
         while(hoursWorked < 0 || hoursWorked > 60) {
             System.out.println("Incorrected, must be below 0 and above 60:");
             hoursWorked = scanner.nextDouble();
-            if (hoursWorked > 39) {
+        }
+
+        if (hoursWorked > 39) {
                 overTime = hoursWorked - 39;
                 hoursWorked = 39;
                 totalHours = overTime + hoursWorked;
             }
+
+        else {
+            totalHours = hoursWorked;
         }
 
         System.out.println("Enter hourly rate:");
